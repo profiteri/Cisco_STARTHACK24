c16bae0acaa3e13523e82a564f20e5d2465d0fc9
Valery Tsarou <valery.tsarou@tum.de>
2024-03-21 16:45:48 +0100

    add humidity

--- src/humidity.py
+++ src/humidity.py
@@ -1,7 +1,78 @@
 import globals
 
 def filter_humidity_events(raw_ds):
-    pass
+
+    events_at_timestamp = dict()
+    total_events = 0
+    for obj in raw_ds:
+        
+        if obj["eventType"] != "IOT_TELEMETRY":
+            continue
+
+        if "iotTelemetry" not in obj:
+            continue
+
+        if "humidity" not in obj["iotTelemetry"]:
+            continue
+
+        if "humidityInPercentage" not in obj["iotTelemetry"]["humidity"]:
+            continue
+
+        if obj["iotTelemetry"]["humidity"]["humidityInPercentage"] == -1:
+            continue
+
+        if "detectedPosition" not in obj["iotTelemetry"]:
+            continue
+
+        total_events += 1
+
+        location_update = obj["iotTelemetry"]["detectedPosition"]
+        pos_x = location_update["xPos"]
+        pos_y = location_update["yPos"]
+
+        new_obj = dict()
+        new_obj["xPos"] = pos_x
+        new_obj["yPos"] = pos_y
+        new_obj["humidity"] = obj["iotTelemetry"]["humidity"]["humidityInPercentage"]
+
+        if globals.MIN_X == None or globals.MIN_X > pos_x:
+            globals.MIN_X = pos_x
+        if globals.MAX_X == None or globals.MAX_X < pos_x:
+            globals.MAX_X = pos_x
+        if globals.MIN_Y == None or globals.MIN_Y > pos_y:
+            globals.MIN_Y = pos_y
+        if globals.MAX_Y == None or globals.MAX_Y < pos_y:
+            globals.MAX_Y = pos_y
+
+        if globals.TIMESTAMP_GRANULARITY > 0:
+            ts = int(str(obj["recordTimestamp"])[:-globals.TIMESTAMP_GRANULARITY])
+        else:
+            ts = obj["recordTimestamp"]
+        if ts not in events_at_timestamp:
+            events_at_timestamp[ts] = []
+        events_at_timestamp[ts].append(new_obj)
+
+    print(f"humidity: {len(events_at_timestamp)}/{total_events}")
+    print(f"x: {globals.MIN_X} - {globals.MAX_X}")
+    print(f"y: {globals.MIN_Y} - {globals.MAX_Y}")
+
+    return events_at_timestamp    
 
 def prepare_humidity_data(events_at_timestamp):
-    pass
+    
+    all_data = []
+    events_at_timestamp = dict(sorted(events_at_timestamp.items()))
+
+    for events in events_at_timestamp.values():
+        df = {"x": list(), "y": list(), "Humidity": list()}
+        for event in events:
+            x = int(event["xPos"])
+            y = int(event["yPos"])
+            val = int(event["humidity"])
+            df["x"].append(x)
+            df["y"].append(y)
+            df["Humidity"].append(val)
+
+        all_data.append(df)
+
+    return all_data
--- src/main.py
+++ src/main.py
@@ -2,12 +2,15 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle as pkl
+import pandas as pd
 import os
 import json
 from matplotlib.widgets import Slider
 
 import globals
 import heatmap
+import humidity
+#import illuminocity
 
 globals.init()
 
@@ -21,24 +24,28 @@ def load_dataset():
 def draw_kdeplot(all_data):
     return sns.kdeplot(data=all_data[current_index], x="x", y="y", bw_adjust=0.2, cmap="Reds", fill=True, alpha=0.4, ax=ax)
 
-def initialize_heatmap(all_data, ax, bg_image):
+def draw_humudity(all_data):
+    glue = pd.DataFrame.from_dict(all_data[current_index]) 
+    print(glue)
+    sns.scatterplot(data=glue, x='x', y='y', hue='Humidity', s=300, palette=sns.color_palette("ch:s=.25,rot=-.25", as_cmap=True), alpha=0.3, ax=ax)
+
+def initialize_heatmap(all_data):
 
     current_index = 0
-
-    # Plot the background image
-    ax.imshow(bg_image,
-            aspect='auto',
-            extent=[globals.MIN_X, globals.MAX_X, globals.MIN_Y, globals.MAX_Y])
-
     # Plot the initial KDE plot
     draw_kdeplot(all_data)
-    ax.axis('off')
+
+def initialize_humidity(all_data):
+
+    current_index = 0
+    draw_humudity(all_data)
 
 # Prepare generic data
 raw_ds              = load_dataset()
 
 # Prepare concrete data
 events_at_timestamp_heatmap = heatmap.filter_heatmap_events(raw_ds)
+events_at_timestamp_humidity = humidity.filter_humidity_events(raw_ds)
 
 # Prepare heatmap data
 global all_heatmap_data
@@ -50,22 +57,28 @@ global all_illumisocity_data
 
 # Prepare humidity data
 global all_humidity_data
-# ...
+all_humidity_data = humidity.prepare_humidity_data(events_at_timestamp_humidity)
 
 # Create the initial figure and axis
 fig, ax = plt.subplots()
 current_index = 0
 
+# Load bg image
+bg_image = plt.imread('../data/test_background.png')
+
+# Plot the background image
+ax.imshow(bg_image,
+        aspect='auto',
+        extent=[globals.MIN_X, globals.MAX_X, globals.MIN_Y, globals.MAX_Y])
+ax.axis('off')
+
 # Set the limits for the x and y axes to prevent the graph from changing height
 ax.set_xlim(globals.MIN_X, globals.MAX_X)
 ax.set_ylim(globals.MIN_Y, globals.MAX_Y)
 
-# Load bq image
-bg_image = plt.imread('../data/test_background.png')
-
-initialize_heatmap(all_heatmap_data, ax, bg_image)
+initialize_heatmap(all_heatmap_data)
 #initialize_illumisocity()
-#initialize_humidity()
+initialize_humidity(all_humidity_data)
 
 # Add a slider for timeline navigation
 ax_slider = plt.axes([0.2, 0.05, 0.65, 0.03])  # [left, bottom, width, height]
@@ -74,13 +87,15 @@ slider = Slider(ax_slider, 'Timeline', 0, len(all_heatmap_data) - 1, valinit=0, 
 # Update function for slider
 def update(val):
 
-    if globals.HEATMAP_SELECTED == True:
-        global current_index
-        current_index = int(slider.val)
+    global current_index
 
-        # Remove only the KDE plot, not the background image
-        for artist in ax.collections:
-            artist.remove()        
+    # Remove only the KDE plot, not the background image
+    for artist in ax.collections:
+        print("remove: ", artist)
+        artist.remove()      
+
+    if globals.HEATMAP_SELECTED == True:
+        current_index = int(slider.val)      
             
         # Plot the KDE plot without clearing the background image
         draw_kdeplot(all_heatmap_data)
@@ -88,7 +103,18 @@ def update(val):
         # Ensure the x and y limits remain the same after updating the plot
         ax.set_xlim(globals.MIN_X, globals.MAX_X)
         ax.set_ylim(globals.MIN_Y, globals.MAX_Y)
-        plt.draw()
+
+    if globals.HUMIDITY_SELECTED == True:
+        current_index = int(slider.val)   
+            
+        # Plot the KDE plot without clearing the background image
+        draw_humudity(all_humidity_data)
+        
+        # Ensure the x and y limits remain the same after updating the plot
+        ax.set_xlim(globals.MIN_X, globals.MAX_X)
+        ax.set_ylim(globals.MIN_Y, globals.MAX_Y)
+
+    plt.draw()
 
 slider.on_changed(update)
 
