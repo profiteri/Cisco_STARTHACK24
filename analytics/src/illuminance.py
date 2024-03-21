dac5351288650c5a7e038849d8ff97c74c653720
Artem Melnychuk <melnychuk.letter@gmail.com>
2024-03-21 22:26:13 +0100

    illum

--- analytics/src/illuminance.py
+++ analytics/src/illuminance.py
@@ -1,15 +1,71 @@
 import globals
+def filter_illuminance_events(raw_ds):
 
-# def filter_illuminocity_events(raw_ds):
-#     pass
+    events_at_timestamp = dict()
+    total_events = 0
+    for obj in raw_ds:
+        
+        if obj["eventType"] != "IOT_TELEMETRY":
+            continue
 
-def prepare_illuminance_data(dataset):
+        if "iotTelemetry" not in obj:
+            continue
+
+        if "illuminance" not in obj["iotTelemetry"]:
+            continue
+
+        if "detectedPosition" not in obj["iotTelemetry"]:
+            continue
+
+        total_events += 1
+
+        location_update = obj["iotTelemetry"]["detectedPosition"]
+        pos_x = location_update["xPos"]
+        pos_y = location_update["yPos"]
+
+        new_obj = dict()
+        new_obj["xPos"] = pos_x
+        new_obj["yPos"] = pos_y
+        new_obj["illuminance"] = obj["iotTelemetry"]["illuminance"]
+
+        if globals.MIN_X == None or globals.MIN_X > pos_x:
+            globals.MIN_X = pos_x
+        if globals.MAX_X == None or globals.MAX_X < pos_x:
+            globals.MAX_X = pos_x
+        if globals.MIN_Y == None or globals.MIN_Y > pos_y:
+            globals.MIN_Y = pos_y
+        if globals.MAX_Y == None or globals.MAX_Y < pos_y:
+            globals.MAX_Y = pos_y
+
+        if globals.TIMESTAMP_GRANULARITY > 0:
+            ts = int(str(obj["recordTimestamp"])[:-globals.TIMESTAMP_GRANULARITY])
+        else:
+            ts = obj["recordTimestamp"]
+        if ts not in events_at_timestamp:
+            events_at_timestamp[ts] = []
+        events_at_timestamp[ts].append(new_obj)
+
+    print(f"illuminance: {len(events_at_timestamp)}/{total_events}")
+    print(f"x: {globals.MIN_X} - {globals.MAX_X}")
+    print(f"y: {globals.MIN_Y} - {globals.MAX_Y}")
+
+    return events_at_timestamp    
+
+
+def prepare_illuminance_data(events_at_timestamp):
     all_data = []
-    for event in dataset:
-        if event['eventType'] == 'IOT_TELEMETRY' and 'illuminance' in event['iotTelemetry']:
-            illuminance_data = event['iotTelemetry']['illuminance']
-            xPos = event['iotTelemetry']['detectedPosition']['xPos']
-            yPos = event['iotTelemetry']['detectedPosition']['yPos']
-            illum_value = illuminance_data.get('value', 0)  # Default to 0 if 'value' is not present
-            all_data.append({'x': xPos, 'y': yPos, 'illum': illum_value})
+    events_at_timestamp = dict(sorted(events_at_timestamp.items()))
+    for events in events_at_timestamp.values():
+        df = {"x": list(), "y": list(), "illuminance": list()}
+        for event in events:
+            x = int(event["xPos"])
+            y = int(event["yPos"])
+            print(event["illuminance"])
+            val = int(event["illuminance"]['value'])
+            df["x"].append(x)
+            df["y"].append(y)
+            df["illuminance"].append(val)
+
+        all_data.append(df)
+
     return all_data
